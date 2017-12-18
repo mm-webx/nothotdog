cf8f738c3e49a9692969bd7501ff68f37af8f276
mm-webx <mm@webx.pl>
2017-12-18 10:24:24 +0100

    add postgres

--- README.md
+++ README.md
@@ -5,20 +5,31 @@
 - Python: 3.5+
 - RabbitMQ (celery)
 - Redis (django-channels)
+- Posgres (because of multitasking)
 
 ### Tests:
 - Run `pytest`
 
 ### Installation:
-- Install rabbit & redis servers `$ sudo apt-get install rabbitmq-server redis-server`
+- Install rabbit & redis servers & postgres `$ sudo apt-get install rabbitmq-server redis-server libpq-dev postgresql postgresql-contrib`
+- Create Postgres database
 - Create virtualenv with required python version
 - Activate virtualenv `source env/bin/activate`
 - Install requirements `pip install -r requirements.txt`
-- Set environments variables:
+- Set environments variables or copy `core/settings.py`:
 ```
 # SECURITY WARNING: keep the secret key used in production secret!
 export DJANGO_SECRET_KEY="o_$thkj$jki@cn2^l=wo=$=a9pp2e&n-ez4+idf6occeht+(i&"
 
+# Database settings
+DJANGO_DB_NAME="hotdog"
+DJANGO_DB_USER="postgres"
+DJANGO_DB_PASSWORD="postgres"
+DJANGO_DB_HOST="127.0.0.1"
+DJANGO_DB_PORT="5432"
+```
+- Set environment variable for GOOGLE SERVICE
+```
 # Google service account file
 # https://cloud.google.com/docs/authentication/getting-started#auth-cloud-implicit-python
 export GOOGLE_APPLICATION_CREDENTIALS=PATH/app-0000.json
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -66,10 +66,20 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DJANGO_DB_NAME', 'hotdog'),
+        'USER': os.environ.get('DJANGO_DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DJANGO_DB_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('DJANGO_DB_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('DJANGO_DB_PORT', '5432'),
     }
 }
 
@@ -161,4 +171,6 @@ CHANNEL_LAYERS = {
 google_api_key = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS', None)
 print('GOOGLE_APPLICATION_CREDENTIALS:', google_api_key)
 if google_api_key is None:
-    raise ValueError('GOOGLE_APPLICATION_CREDENTIALS missing in env!')
+    print('GOOGLE_APPLICATION_CREDENTIALS missing in env!')
+    print('GOOGLE_APPLICATION_CREDENTIALS missing in env!')
+    print('GOOGLE_APPLICATION_CREDENTIALS missing in env!')
